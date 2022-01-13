512065a3eb4232527a68911d8dc8a2657ef47e35
lonewolf053 <prime.05@tutanota.com>
2022-01-13 09:22:45 +0530

    First commit

---
+++ Caesar.py
@@ -0,0 +1,47 @@
+# Caesar cipher
+def encrypt(text,shift):
+    ans = ""
+    for char in text:
+        if char.isupper():
+            ans+=chr((ord(char)+shift-65)%26+65)
+        elif char.islower():
+            ans += chr((ord(char)+shift-97)%26+97)
+        else:
+            ans+=char
+    return ans
+
+def decrypt(text,shift):
+    ans = ""
+    for char in text:
+        if char.isupper():
+            ans+=chr((ord(char)-shift-65)%26+65)
+        elif char.islower():
+            ans += chr((ord(char)-shift-97)%26+97)
+        else:
+            ans+=char
+    return ans
+
+
+text = input("Enter text to encrypt or decrypt: \n")
+
+while True:
+    if "choice" not in globals():
+        try:
+            choice = int(input("Type 1 to encrypt and 2 to decrypt: \n"))
+        except:
+            print("Input invalid, please try again")
+            continue
+
+    try:
+        shift = int(input("Enter Key to Shift"))
+        break
+    except:
+        print("Input invalid, please try again")
+        
+    
+if choice == 1:
+    print(encrypt(text,shift))
+elif choice == 2:
+    print(decrypt(text,shift))
+        
+        
